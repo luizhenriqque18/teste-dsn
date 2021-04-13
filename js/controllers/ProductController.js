d74a6b182f48cf1621f37c837ac1ab7c8eff014b
Luiz Henrique O. de Souza <luizhenriqque15@gmail.com>
2021-04-12 23:49:29 -0400

    add comments

--- js/controllers/ProductController.js
+++ js/controllers/ProductController.js
@@ -1,79 +1,117 @@
+/**
+ * class responsible for managing operations in html and javascript
+ */
 class ProductController {
-    constructor() {
-        let $ = document.querySelector.bind(document);
-        this._productList = new ProductList();
-        this._pagination = new Pagianation();
-        this._productView = new ProductView($('#product-view'));
-        this._btnNextPage = $('#btn-next-page');
-        this._loader = $('#loader');
-        this.getInit();
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._productList = new ProductList();
+    this._pagination = new Pagianation();
+    this._productView = new ProductView($("#product-view"));
+    this._btnNextPage = $("#btn-next-page");
+    this._loader = $("#loader");
+    this.getInit();
+  }
+
+  /**
+   * method starts product listing operations
+   *
+   * @returns {void};
+   */
+  getInit() {
+    this.setLoader();
+    this._getProduct()
+      .then((response) => {
+        const { products, nextPage } = response;
+        this._productList.addList(products);
+        this._productView.update(this._productList);
+        this._pagination.update(nextPage);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => {
+        this.setLoader(false);
+      });
+  }
+
+  /**
+   * method starts operation to search for new product list
+   *
+   * @returns {void};
+   */
+  getNextPage(event) {
+    this.setLoader();
+    this.setDisabledBtn();
+
+    let path = this._pagination.nextPage.split("/");
+
+    if (path.length < 0) throw new Error("Próxima página não encontrada.");
+
+    this._getProductPage(path[1])
+      .then((response) => {
+        const { products, nextPage } = response;
+        this._productList.addList(products);
+        this._productView.update(this._productList);
+        this._pagination.update(nextPage);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => {
+        this.setLoader(false);
+        this.setDisabledBtn(false);
+      });
+  }
+
+  /**
+   * Wrapping method request products
+   *
+   * @returns {Promise}
+   */
+  async _getProduct() {
+    let service = new ProductService();
+    let response = await service.getProduct();
+    return response;
+  }
+
+  /**
+   * Wrapping method request next products
+   *
+   * @returns {Promise}
+   */
+  async _getProductPage(url) {
+    let service = new ProductService();
+    let response = await service.getProductByPage(url);
+
+    return response;
+  }
+
+  /**
+   * Loader state managed
+   *
+   * @param {*} status
+   *
+   * @returns {void}
+   */
+  setLoader(status = true) {
+    if (status) this._loader.classList.add("loader");
+    else this._loader.classList.remove("loader");
+  }
+
+  /**
+   * Disabled button state managed
+   *
+   * @param {*} status
+   *
+   * @returns {void}
+   */
+  setDisabledBtn(status = true) {
+    if (status) {
+      this._btnNextPage.classList.add("btn-blocked");
+      this._btnNextPage.disabled = true;
+    } else {
+      this._btnNextPage.classList.remove("btn-blocked");
+      this._btnNextPage.disabled = false;
     }
-
-    getInit() {
-        this.setLoader();
-        this._getProduct()
-            .then( response => {
-                const {products, nextPage} = response;
-                this._productList.addList(products);
-                this._productView.update(this._productList);
-                this._pagination.update(nextPage);
-            })
-            .catch(error=>{
-                console.log('error', error);
-            }).finally(()=>{
-                this.setLoader(false);
-            });
-    }
-
-    getNextPage(event) {
-        this.setLoader();
-        this.setDisabledBtn();
-
-        let path = this._pagination.nextPage.split("/");
-
-        if(path.length < 0) throw new Error("Próxima página não encontrada.");
-        
-        this._getProductPage(path[1])
-            .then( response => {
-                const {products, nextPage} = response;
-                this._productList.addList(products);
-                this._productView.update(this._productList);
-                this._pagination.update(nextPage);
-            })
-            .catch(error=>{
-                console.log('error', error);
-            }).finally(()=>{
-                this.setLoader(false);
-                this.setDisabledBtn(false);
-            });
-    }
-
-    async _getProduct() {
-        let service = new ProductService();
-        let response = await service.getProduct();
-        return response;
-    }
-
-    async _getProductPage(url) {
-        let service = new ProductService();
-        let response = await service.getProductByPage(url);
-
-        return response;
-    }
-
-    setLoader(status = true) {
-        if(status) this._loader.classList.add("loader");
-        else this._loader.classList.remove("loader");
-    }
-
-    setDisabledBtn(status = true){
-        if(status) {
-            this._btnNextPage.classList.add("btn-blocked");
-            this._btnNextPage.disabled = true;
-        }
-        else {
-            this._btnNextPage.classList.remove("btn-blocked");
-            this._btnNextPage.disabled = false;
-        }
-    }
+  }
 }
