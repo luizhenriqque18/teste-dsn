c12db58cf2bb677dffd467c10c3f9cc57a05228b
Luiz Henrique O. de Souza <luizhenriqque15@gmail.com>
2021-04-13 20:45:08 -0400

    fix convert value float

--- js/views/ProductView.js
+++ js/views/ProductView.js
@@ -1,31 +1,45 @@
 class ProductView extends View {
-    template(model) {
-        let products = model._productList;
+  template(model) {
+    let products = model._productList;
 
-        let result = products.map( product =>
-            `<li class="product-item flex-item-3">
+    let result = products
+      .map(
+        (product) =>
+          `<li class="product-item flex-item-3">
                 <figure class="">
                     <img src="http://${product.image}" alt="${product.name}">
                     <figcaption>${product.name}</figcaption>
                 </figure>
                 <div>
                     <p>${product.description}</p>
-                    <span>De: R$${product.oldPrice} </span>
-                    <span><b>Por: R$${product.price}</b></span>
-                    <span>ou ${product.installments.count}x de R$${product.installments.value}</span>
+                    <span>De: ${this.convertForPTBR(product.oldPrice)} </span>
+                    <span><b>Por: ${this.convertForPTBR(
+                      product.price
+                    )}</b></span>
+                    <span>
+                      ou ${ product.installments.count}x de ${this.convertForPTBR(product.installments.value)}
+                    </span>
                     <button>Comprar</button>
                 </div>
             </li>`
-        ).join('');
+      )
+      .join("");
 
-        if(result.length === 0) {
-            result = `
+    if (result.length === 0) {
+      result = `
             <li class="flex-item-3">
                 <h1 style="text-align: center;">Lista de produtos vazia!!!</h1>
             </li>
             `;
-        }
-        
-        return result;
     }
+
+    return result;
+  }
+
+  convertForPTBR(value = 0.0) {
+    return value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
 }
